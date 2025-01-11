6a0b9b7f3c32a0e0b06e1b1b410ca1608b265fe4
morris-bedroom <marrocco001@gmail.com>
2025-01-11 11:39:30 +0000

    all

--- try.c
+++ try.c
@@ -1,9 +1,33 @@
-// 123  Creat an ADD function 456
-// 123 Create a while loop COUNTDOWN from n to 0 45
-// 123456  Create a FOR-LOOP counting from 0 to n 6
+//FOR THE HOLIDAY PERIOD, REVISE EXISTING SIMPLER TASKS BELOW BY RE-CREATING THE M AND TESTING THEM. REFER TO YOUR CREATED FILES IF YOU GET STUCK. 
+
+//ALSO, LOOK UP (VIA GOOGLE) HOW THE WALL BUILDING LOOPS WORK. 
+
+  //(HOLIDAY1) 123  Creat an ADD function 456
+// (HOLIDAY1AB) 123 Create a while loop COUNTDOWN from n to 0 45
+
+ // (HOLIDAY1A) 123456  Create a FOR-LOOP counting from 0 to n 6
 // 12 Test if user entered YES_OR_NO 345
 // 1234 COMPARE two numbers. Test which is greater.5
 //1 Asks someone's name and GREET them. 2345
-   // 1bCreate a LOOP1 to print n numbers of a char in a row.23456 c
+// 12cCreate a LOOP1 to print n numbers of a char in a row.23456 
 // Build a LOOP2 that builds a wall of hashes 4 wide + 4 high 123456abc
 
+// 2  Creat an ADD function23456
+
+
+// (HOLIDAY1C) 123 Create a while loop COUNTDOWN from n to 0 45
+//WHILE
+
+#include<stdio.h>
+#include<cs50.h>
+
+int main ()
+{
+	int a=get_int("Number ");
+	do
+	{
+		printf("%i",a);
+		a--;
+	}
+	while(a > 0);
+}
