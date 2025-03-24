9a2174d77d3a659e64e392ebe3ab94683b126eea
morris-bedroom <marrocco001@gmail.com>
2025-03-24 20:33:30 +0000

    All

--- try.c
+++ try.c
@@ -5,12 +5,16 @@
 //Type your code here
 
 
+
+
+
 // 1234  Creat an ADD function 56
 //  123 COUNTDOWN from n to 0 45 
 // 2B Create COUNT_UP TO count from 0 to n 2BC 3456 
 //  3 Test if user entered Y_OR_N 456
+// 12345 COMPARE two numbers. Test which is greater.6
+// 2 Do a TERNARY if else comparing two chars 23456
 
-// (HOLIDAY1)1234 COMPARE two numbers. Test which is greater.(H)5
 // (HOILDAY1) 1 Asks someone's name and GREET them. 2345 (H)
 //  1 Test if user entered the strings YES_OR_NO 2345  
 // 1c2c Create CHAR_COUNTING to print n numbers of a char in a row.3456 
