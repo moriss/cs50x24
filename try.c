297950247b105a4e7b629b755838d10b021022b4
morris-bedroom <marrocco001@gmail.com>
2025-04-07 19:58:32 +0100

    all

--- try.c
+++ try.c
@@ -1,9 +1,7 @@
-//Complete all possible versions of loops (ABC) from 1 to 6.
-//NB: Despite having apparantly downloaded the new git library version, You still can't use GetInt. It is throwing up an error.
-
+//Complete all possible types of loops (ABC) from 1 to 6.
+//Create 3 versions of each code instruction (v1. v2. and v3)
 
 //Type your code here
-
 
 
 
@@ -14,12 +12,10 @@
 //  3 Test if user entered Y_OR_N 456
 // 12345 COMPARE two numbers. Test which is greater.6
 // 2 Do a TERNARY if else comparing two chars 23456
+// 2.v1 Asks someone's name and GREET them. 2345 
+//  2.v2 Test if user entered the strings YES_OR_NO 2.v2 3456  
 
-// (HOILDAY1) 1 Asks someone's name and GREET them. 2345 (H)
-//  1 Test if user entered the strings YES_OR_NO 2345  
-// 1c2c Create CHAR_COUNTING to print n numbers of a char in a row.3456 
+// 3b Create CHAR_COUNTING to print n numbers of a char in a row.3456 V1 
 //1a Build a LOOP2 that builds a wall of hashes 4 wide + 4 high 23456bc
 // use CASE with GetInt to categorise the user's number into small,medium or large 123456
-
-
- // 1B Create COUNT_UP TO count from 0 to n 23456 BC
+// 1B Create COUNT_UP TO count from 0 to n 23456 BC
