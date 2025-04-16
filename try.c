bb7d047a07ce6fa7f288bc759f3794e06d49c1f4
morris-bedroom <marrocco001@gmail.com>
2025-04-16 21:07:44 +0100

    all

--- try.c
+++ try.c
@@ -1,11 +1,10 @@
 //Complete all possible types of loops (ABC) from 1 to 6.
 //Create 3 versions of each code instruction (v1. v2. and v3)
 //If unsure about last peice of code, repeat attempt 
+// Push your work to git at the end
+
 
 //Type your code here
-//1b Build a LOOP2 that builds a wall of hashes 4 wide + 4 high 23456 V1
-//ON REPEAT
-
 
 
 // 1234  Creat an ADD function 56
@@ -17,7 +16,7 @@
 // 2.v1 Asks someone's name and GREET them. 2345 
 //  2.v2 Test if user entered the strings YES_OR_NO 2.v2 3456  
 // 3c Create CHAR_COUNTING to print n numbers of a char in a row.3456 V1 
-//1b Build a LOOP2 that builds a wall of hashes 4 wide + 4 high 23456 V1
+//1c Build a LOOP2 that builds a wall of hashes 4 wide + 4 high 23456 V1
 
 // 2 use SWTICH to categorise the user's number into small,medium or large 23456 v1
 // 1B Create COUNT_UP TO count from 0 to n 23456 BC
