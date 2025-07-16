cfd2a0dc1165f334c7d4f1686e33b7685becbeb3
morris-bedroom <marrocco001@gmail.com>
2025-07-16 21:26:50 +0100

    all

--- try.c
+++ try.c
@@ -1,4 +1,4 @@
-//Complete all possible types of loops (ABC) from 1 through to 6.
+//Complete all possible types of loops (A(FOR) B(WHILE) C(DO WILE)) from 1 through to 6.
 //Create 3 versions of each code instruction (v1. v2. and v3) BUT DON'T MOVE TO V2 EETC UNTIL YOU ARE VERY COMFORTABLE WITH V1
 // v2 and v3 require more sophisticated features such as fault testing etc. check with chat.
 //If unsure about last peice of code, repeat attempt 
@@ -9,9 +9,9 @@
 
 
 
-
 // 6  Creat an ADD function 6 v1
-//  4 COUNTDOWN from n to 0 456  v1
+//  4B COUNTDOWN from n to 0 456  v1
+
 // 3a Create COUNT_UP TO count from 0 to n 3a 456 v1 
 //  3 Test if user entered Y_OR_N 3456 v1
 // 1  COMPARE two numbers. Test which is greater.123456 v2
@@ -21,9 +21,8 @@
 // 4a Create CHAR_COUNTING to print n numbers of a char in a row.456 V1 
 // 2a Build a LOOP2 that builds a wall of hashes 4 wide + 4 high 23456 V1
 // 4 use SWTICH to categorise the user's number into small,medium or large 456 v1
-
-// 2B Create COUNT_UP TO count from 0 to n 23456 BC v1
-//2 Calculate users WAGE  including time and a half for hours over 40 23456 v1
+// 3A Create COUNT_UP TO count from 0 to n 3456 BC v1
+//3 Calculate users WAGE  including time and a half for hours over 40 3456 v1
 
 
 
